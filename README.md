d54afd0c120f5478e68ec3553542a8f4c365a185
zhangmj <15669993907@163.com>
2022-10-21 23:08:03 +0800

    引入vant，制作tab-bar，并总结引入第三库修改样式的几种方法

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# mj-trip
-使用Vue3新技术，制作一个H5
+# 一、项目初始化步骤:
+
+1、
+
+2、
+
+3、
+
+4、
+
+5、
+
+6、
+
+### 二、引入 vant
+
+1、 [Vant 官方网站](https://vant-contrib.gitee.io/vant/#/zh-CN)
+2、
--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="tab-bar">
-    <template v-for="(item, index) in tabbarData">
-      <div class="tab-bar-item" :class="{ active: currentIndex == index }" @click="itemClick(index, item)">
-        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" />
-        <img v-if="currentIndex === index" :src="getAssetURL(item.imageActive)" />
-        <span class="text"> {{ item.text }}</span>
-      </div>
-    </template>
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path">
+          <span class="text"> {{ item.text }}</span>
+          <template #icon>
+            <img :src="currentIndex === index ? getAssetURL(item.imageActive) : getAssetURL(item.image)" />
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
   </div>
 </template>
 
@@ -14,43 +17,13 @@
 import tabbarData from '@/assets/data/tabbar.js'
 import { getAssetURL } from '@/utils/load_assets.js'
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-
 const currentIndex = ref(0)
-const router = useRouter()
-const itemClick = (index, item) => {
-  currentIndex.value = index
-  router.push(item.path)
-}
 </script>
 
 <style lang="less" scoped>
 .tab-bar {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-  border-top: 1px solid #f3f3f3;
-
-  .tab-bar-item {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    img {
-      width: 32px;
-    }
-    .text {
-      margin-top: 2px;
-      font-size: 12px;
-    }
-
-    &.active {
-      color: var(--primary-color);
-    }
+  img {
+    height: 28px;
   }
 }
 </style>
