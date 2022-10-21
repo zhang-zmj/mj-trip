f78ff4d198b9bf5f6d8e33d362297bc899668210
zhangmj <15669993907@163.com>
2022-10-21 21:10:43 +0800

    tab-bar的初步搭建

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="app">
     <router-view />
+    <tab-bar />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import tabBar from '@/components/tab-bar/tab-bar.vue'
+</script>
 
 <style scoped></style>
