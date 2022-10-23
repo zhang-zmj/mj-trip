319cd1ecd313af380d2bfe3d5e82bcb7ab3fe040
zhangmj <15669993907@163.com>
2022-10-23 21:25:52 +0800

    日期的创建

--- src/views/home/component/home-location.vue
+++ src/views/home/component/home-location.vue
@@ -1,10 +1,45 @@
 <template>
-  <div class="location">
-    <div class="city" @click="cityClick">{{ currentCity.cityName }}</div>
-    <div class="position" @click="postionClick">
-      <span class="text">我的位置</span>
-      <img src="@/assets/img/home/icon_location.png" />
+  <div class="search-box">
+    <!-- 位置信息 -->
+    <div class="location">
+      <div class="city" @click="cityClick">{{ currentCity.cityName }}</div>
+      <div class="position" @click="postionClick">
+        <span class="text">我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" />
+      </div>
     </div>
+
+    <!-- 日期范围 -->
+    <div class="section date-range bottom-gray-line" @click="showCalendar = true">
+      <div class="start">
+        <div class="date">
+          <span class="tip">入住</span>
+          <span class="time">{{ startDate }}</span>
+        </div>
+        <div class="stay">共{{ stayCount }}晚</div>
+      </div>
+      <div class="end">
+        <div class="date">
+          <span class="tip">离店</span>
+          <span class="time">{{ endDate }}</span>
+        </div>
+      </div>
+    </div>
+
+    <van-calendar
+      v-model:show="showCalendar"
+      type="range"
+      color="#ff9854"
+      :round="false"
+      :show-confirm="false"
+      @confirm="onConfirm"
+    />
+  </div>
+
+  <!-- 价格/人数选择 -->
+  <div class="section price-counter bottom-gray-line">
+    <div class="start">价格不限</div>
+    <div class="end">人数不限</div>
   </div>
 </template>
 
@@ -12,6 +47,9 @@
 import { useRouter } from 'vue-router'
 import useCityStore from '@/stores/modules/city'
 import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
+import { formatMonthDay, getDiffDays } from '@/utils/format_date'
+
 const router = useRouter()
 
 const cityClick = () => {
@@ -37,6 +75,27 @@ const postionClick = () => {
 //获取当前城市
 const cityStore = useCityStore()
 const { currentCity } = storeToRefs(cityStore)
+
+// 日期范围的处理
+const nowDate = new Date()
+const newDate = new Date()
+newDate.setDate(nowDate.getDate() + 1)
+
+const startDate = ref(formatMonthDay(nowDate))
+const endDate = ref(formatMonthDay(newDate))
+const stayCount = ref(getDiffDays(nowDate, newDate))
+
+const showCalendar = ref(false)
+const onConfirm = value => {
+  // 1.设置日期
+  const selectStartDate = value[0]
+  const selectEndDate = value[1]
+  startDate.value = formatMonthDay(selectStartDate)
+  endDate.value = formatMonthDay(selectEndDate)
+  stayCount.value = getDiffDays(selectStartDate, selectEndDate)
+  // 2.隐藏日历
+  showCalendar.value = false
+}
 </script>
 
 <style lang="less" scoped>
@@ -63,6 +122,60 @@ const { currentCity } = storeToRefs(cityStore)
       width: 18px;
       height: 18px;
     }
+  }
+}
+
+.section {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 0 20px;
+  color: #999;
+  height: 44px;
+
+  .start {
+    flex: 1;
+    display: flex;
+    height: 44px;
+    align-items: center;
+  }
+
+  .end {
+    min-width: 30%;
+    padding-left: 20px;
+  }
+
+  .date {
+    display: flex;
+    flex-direction: column;
+
+    .tip {
+      font-size: 12px;
+      color: #999;
+    }
+
+    .time {
+      margin-top: 3px;
+      color: #333;
+      font-size: 15px;
+      font-weight: 500;
+    }
+  }
+}
+
+.date-range {
+  height: 44px;
+  .stay {
+    flex: 1;
+    text-align: center;
+    font-size: 12px;
+    color: #666;
+  }
+}
+
+.price-counter {
+  .start {
+    border-right: 1px solid var(--line-color);
   }
 }
 </style>
